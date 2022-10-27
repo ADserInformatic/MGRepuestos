bd0102250403bfc03876abdac88849e3ab01233a
nahad <alijihernandez@gmail.com>
2022-10-27 03:53:12 -0300

    antes del pull

--- routes/clients.js
+++ routes/clients.js
@@ -4,41 +4,80 @@ const Clients=require ('../models/client');
 // const fs = require('fs-extra');
 // const format = require('date-fns/format');
 const cron =require ('node-cron')//para programar tarea
+const nodemailer=require('nodemailer')
 
 
 // Find your account sid and auth token in your Twilio account Console.
 //var client = new twilio('MG831f19fb057c965a23e4788103079286', '[Redacted]');
 const accountSid = 'ACb3b1b7a3bf7538f5908756d7dcb42d20'; 
-//const authToken = '84bf2bb58b9d198fde3de0b28c3ab989'; 
-const authToken = '627a72a6f88c8f8d3cf00bf0c15ec4ec'
+const authToken = '87be7a175ca973a11df45420a05a313f7'; 
 const client = require('twilio')(accountSid, authToken); 
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+ //port : 465,
+    secure: false,
+    auth: {
+        user: 'mariagranderepuestos@gmail.com',
+        //pass: 'qnzetcsthpyowuvy'
+        pass: 'cuentaCuenta'
+    }
+});
 
-
-router.post('/senda', (req, res)=>{
+router.post('/senda', async (req, res)=>{
     
  
-        client.messages 
-            .create({ 
-                body: 'Beleeeeeeeen', 
-                from: 'whatsapp:+14155238886',       
-                to: 'whatsapp:+5493436222320' 
-            }) 
-            .then(res.json('ok')) 
-            .done();
+        // client.messages 
+        //     .create({ 
+        //         body: 'Paga la prata', 
+        //         from: 'whatsapp:+14155238886',       
+        //         to: 'whatsapp:+5493436222320' 
+        //     }) 
+        //     .then(res.json('ok')) 
+        //     .done();
+
+        try{
+            const correo = await transporter.sendMail({
+                from: "mariagranderepuestos@gmail.com",
+                to: 'diegokemer26@gmail.com',
+                subject: "¡¡ATENCION!! Posee un saldo en Maria Grande Repuestos",
+                text: "Maria Grande Repuestos",
+                html: `Buenos días Diego, nos comunicamos de María Grande Repuestos para  recordarle que Ud. posee una saldo de $mucha, por favor comunicarse a la brevedad.`
+            })
+
+            console.log('succefully')
+        }catch(err) {console.log(err )}
+
 })
 
 const job = cron.schedule('0 10 3 * *', async () => {//TAREA A LAS 10 EN PUNTO DEL DIA 3 DE CADA MES DURANTE EL AÑO
     const clientes = await Clients.find()
     for (let i = 0; i < clientes.length; i++) {//CALCULO DEUDA POR CADA CLIENTE
         if(clientes[i].deuda>0){ //CALCULO SI TIENE UNA DEUDA
-            client.messages 
-            .create({ 
-            body: `Buenos días ${clientes[i].name}, nos comunicamos de María Grande Repuestos para  recordarle que Ud. posee una saldo de $${clientes[i].deuda}, por favor comunicarse a la brevedad.`, 
-            from: 'whatsapp:+14155238886',       
-            to: `whatsapp: ${+clientes[i].cellphone} `
-            }) 
-            .then(res.json('ok')) 
-            .done();
+           
+           //ENVIO WHATSAPP
+            // client.messages 
+            // .create({ 
+            // body: `Buenos días ${clientes[i].name}, nos comunicamos de María Grande Repuestos para  recordarle que Ud. posee una saldo de $${clientes[i].deuda}, por favor comunicarse a la brevedad.`, 
+            // from: 'whatsapp:+14155238886',       
+            // to: `whatsapp: ${+clientes[i].cellphone} `
+            // }) 
+            // .then(res.json('ok')) 
+            // .done();
+
+            //ENVIO CORREO ELECTRONICO
+            
+            try{
+                const correo = await transporter.sendMail({
+                    from: "mariagranderepuestos@gmail.com",
+                    to: req.body.emaildestino,
+                    subject: "¡¡ATENCION!! Posee un saldo en Maria Grande Repuestos",
+                    text: "Maria Grande Repuestos",
+                    html: `Buenos días ${clientes[i].name}, nos comunicamos de María Grande Repuestos para  recordarle que Ud. posee una saldo de $${clientes[i].deuda}, por favor comunicarse a la brevedad.`
+                })
+                console.log('succefully')
+            }catch(err) {console.log(err )}
+
         }        
     }
 }, {
@@ -129,7 +168,7 @@ router.post('/AddPay/:id',async(req,res)=>{
     const id=req.params.id
     const cliente= await Clients.findOne({_id:id});
 
-    if(cliente){
+    if(!cliente){res.json({error:true, mensaje: "codigo",message: "no encontro cliente"})}
     cliente.pays.push(req.body.data);
 
     cliente.deuda-=req.body.data.entrega;
@@ -147,8 +186,7 @@ router.post('/AddPay/:id',async(req,res)=>{
              message: error
          })
      }
-    }
-    res.json({error:true, mensaje: "codigo",message: "no encontro cliente"})
+    
 }
 )
 module.exports = router;
